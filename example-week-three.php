5fbefed24334003afdc574a3d3f2424f0ee38dab
Armando G <armgar@gmail.com>
2018-01-31 15:57:31 -0500

    Added examples of conditional statements

--- example-week-three.php
+++ example-week-three.php
@@ -6,7 +6,46 @@
 	</head>
 	<body>
 		<?php
-			
+		$tempf = 88;
+		$sky = "Sunny!";
+		
+		# if statement
+		if ($tempf >= 32)
+		{
+			echo "Good news! It's not freezing.";
+		}
+		
+		if ($tempf < 32)
+		{
+			echo "Oh lordt! It's freezing! Watch out!";	
+		}
+		
+		
+		# elseif statement
+		if ($tempf >= 32 && $tempf  <= 80)
+		{
+			echo "<p>Good news! It's not freezing.</p>";
+		}
+		elseif ($tempf > 80)
+		{
+			echo "<p>Way too hot out.</p>";	
+		}
+		
+		# Switch statement
+		
+		switch ($sky) 
+		{
+			case "A little cloudy!":
+				echo $sky;
+				break;
+			case "Sunny!":
+				echo ":)";
+				break;
+			default:
+				echo "No idea.";
+				break;
+		}
+		
 		?>
 	</body>
 </html>
