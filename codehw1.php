b9d284c6ea3a43c60a907f5a1e350eae3634801e
Armando G <armgar@gmail.com>
2018-02-07 10:01:05 -0500

    Updated file names to reflect corrections

--- codehw1.php
+++ codehw1.php
@@ -2,183 +2,84 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Code Homework #1</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Code Homework #1 for LIS-638 Web Development">
+		<title>Code Homework #1&mdash;Armando Garcia</title>
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<style>
+			body {
+				font-size: 18px;
+				color: #555555;
+				background-color: #fffafa;
+				font-family: 'Montserrat', sans-serif;
+				margin: auto;
+				padding: 25px;
+				max-width: 960px;
+			}
+			h1 {
+				text-transform: uppercase;
+			}
+			h1, h2 { 
+				color: #ff6347;
+				padding: 5px 0;
+			}
+			strong {
+				font-size: 1.1em;
+			}
+			hr {
+				max-width: 150px;
+				margin: 50px auto;
+				border: 2px solid;
+				opacity: 0.5;
+			}
+		</style>
 	</head>
 	<body>
-		<h1>Code Homework 1</h1>
-		<h2>Challenge 1: Correct Change</h2>
-
 		<?php
+		echo "<h1>Code Homework 1</h1>";
+		echo "<h2>Challenge 1: <br><strong>Correct Change</strong></h2>";
+	
+		$total_change = 176;
+		$dollars = (int)($total_change  / 100); # get whole number dollars by casting as int
+		$remainder = $total_change % 100; # get leftovers from dollar to move on to next step
+		$quarters = (int) ($remainder / 25); # get whole number quarters by casting as int
+		$remainder = $remainder % 25;
+		$dimes = (int) ($remainder / 10);
+		$remainder = $remainder % 10;
+		$nickels = (int) ($remainder / 5);
+		$pennies = ($remainder - ($nickels * 5));
 
-		$total_change = 293;
-		$total_cents = $total_change % 100;
-		$dollars = ($total_change - $total_cents) / 100;
+		echo "<p><strong>You are due $total_change cents back in change total.</strong></p>";
+		echo "<p>You are due back $dollars dollar(s), $quarters quarter(s), $dimes dime(s), $nickels nickel(s), and $pennies cent(s).</p>";
 
-		/* 
-		This crazy equation did not work
-		$quarters = (($total_cents / 25 * 100) - ($total_cents % 25)) / 100;
-		*/
+		echo "<hr>"; # Challenge 2 starts here ****************
 
-		# QUARTERS 
-		if ($total_cents < 25)
-		{
-			$quarters_left = 0;
-		}
-		elseif (($total_cents >= 25) && ($total_cents < 50))
-		{
-			$quarters_left = 1;
-		}
-		elseif (($total_cents >= 50) && ($total_cents < 75))
-		{
-			$quarters_left = 2;
-		}
-		elseif (($total_cents >= 75) && ($total_cents < 100))
-		{
-			$quarters_left = 3;
-		}
-		
-		# How much change is left after you subtract the quarters?
-		$total_less_quarters = $total_cents - ($quarters_left * 25);
+		echo "<h2>Challenge 2: <br><strong>99 Bottles of Beer</strong></h2>";
 
-		# DIMES
-		if ($total_less_quarters < 10)
-		{
-			$dimes_left = 0;
-		}
-		elseif (($total_less_quarters >= 10) && ($total_less_quarters < 20))
-		{
-			$dimes_left = 1;
-		}
-		elseif (($total_less_quarters >= 20) && ($total_less_quarters < 30))
-		{
-			$dimes_left = 2;
-		}
-
-		# How much change is left after you subtract the dimes?
-		$total_less_dimes = $total_cents - ($quarters_left * 25) - ($dimes_left * 10);
-
-		# NICKELS
-		if ($total_less_dimes < 5)
-		{
-			$nickels_left = 0;
-		}
-		elseif (($total_less_dimes >= 5) && ($total_less_dimes < 10))
-		{
-			$nickels_left = 1;
-		}
-
-		# How much change is left after you subtract the nickels? This is the final cents count.
-		$pennies_left = $total_cents - ($quarters_left * 25) - ($dimes_left * 10) - ($nickels_left * 5);
-
-
-		echo "<p>You are due $total_change cents back in change total.</p>";
-		echo "<p>You are due back $dollars dollar(s), $quarters_left quarter(s), $dimes_left dime(s), $nickels_left nickel(s), and $pennies_left cent(s).</p>";
-
-		echo "<hr>";
-
-		# Challenge 2 starts here ****************
-
-		echo "<h2>Challenge 2: 99 Bottles of Beer</h2>";
-
-		$count = 3;
+		$count = 5;
 		$count_next = $count - 1;
 
-		for ($count_input = $count ; $count_input > 2 ; --$count_input)
+		for ($count_input = $count ; $count_input >= 0 ; --$count_input )
 		{
-			echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
-			Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
+			if ($count_input >= 3) {
+				echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
+				Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
+				--$count_next;
+			}
+			if ($count_input == 2) {
+				echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
+				Take one down, pass it around, 1 bottle of beer on the wall.</p>";
+			}
+			if ($count_input == 1) {
+				echo "<p>1 bottle of beer on the wall, 1 bottle of beer.<br>
+				Take one down, pass it around, 0 bottles of beer on the wall.</p>";
+			}
+			if ($count_input == 0) {
+				echo "<p>No more bottles of beer on the wall, no more bottles of beer.<br>
+				We've taken them down and passed them around; now we're drunk and passed out!</p>";
+			}
 		}
-
-		if (($count_input <= 2) && ($count_input > 1))
-		{
-			echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
-			Take one down, pass it around, $count_next bottle of beer on the wall.</p>";
-			--$count_input;
-			--$count_next;
-		}
-
-		if (($count_input <= 1) && ($count_input > 0))
-		{
-			echo "<p>1 bottle of beer on the wall, 1 bottle of beer.<br>
-			Take one down, pass it around, 0 bottles of beer on the wall.</p>";
-		}
-
-		for ($count_input = 0 ; $count_input > -1 ; --$count_input)
-		{
-			echo "<p>No more bottles of beer on the wall, no more bottles of beer.<br>
-			We've taken them down and passed them around; now we're drunk and passed out!</p>";
-		}
-
-
-		/*
-
-		# These loops did not work!
-
-		do
-		{
-			echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
-			Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
-			--$count_input;
-			--$count_next;
-		} while (($count_input < 100) && ($count_input >= 3));
-
-
-		for ($count_input = 1 ; $count_input > 0 ; --$count_input)
-		{
-			echo "<p>1 bottle of beer on the wall, 1 bottle of beer.<br>
-			Take one down, pass it around, 0 bottles of beer on the wall.</p>";
-		}
-
-		for ($count_input = 1 ; $count_input > 0 ; --$count_input)
-		{
-			echo "<p>1 bottle of beer on the wall, 1 bottle of beer.<br>
-			Take one down, pass it around, 0 bottles of beer on the wall.</p>";
-		}
-
-		do 
-		{
-			echo "<p>$count_input bottle of beer on the wall, $count_input bottle of beer.<br>
-			Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
-			--$count_input;
-			--$count_next;
-		} while (($count_input <= 1) && ($count_input > 0));
-
-		
-		for ($count_input <= 2 ; $count_input > 1 ; --$count_input)
-		{
-			echo "<p>$count_input bottles of beer on the wall, $count_input bottles of beer.<br>
-			Take one down, pass it around, $count_next bottle of beer on the wall.</p>";
-			--$count_input;
-			--$count_next;
-		}
-
-		for ($count_input <= 1 ; $count_input > 0 ; --$count_input)
-		{
-			echo "<p>$count_input bottle of beer on the wall, $count_input bottle of beer.<br>
-			Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
-			--$count_input;
-			--$count_next;
-		}
-		
-		for ($count = $count_input ; $count > 0 ; --$count)
-		{
-			echo "<p>$count bottles of beer on the wall, $count bottles of beer.<br>
-			Take one down, pass it around, $count bottles of beer on the wall.</p>";
-		}
-
-		do
-		{
-			echo "<p>$count_input bottle of beer on the wall, $count_input bottle of beer.<br>
-			Take one down, pass it around, $count_next bottles of beer on the wall.</p>";
-		} while ($count_input = 0);
-
-		if ($count = 0)
-		{
-			echo "<p>No more bottles of beer on the wall, no more bottles of beer.<br>
-			We've taken them down and passed them around; now we're drunk and passed out!</p>";
-		}
-		*/
-
 		?>
 	</body>
 </html>
