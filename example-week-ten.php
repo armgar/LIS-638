7c69ddc6ec887e858d7ba5d8ae2b335b5f82f00a
Armando G <armgar@gmail.com>
2018-03-28 17:51:39 -0400

    Update to title

--- example-week-ten.php
+++ example-week-ten.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Code Homework for LIS-638 Web Development">
-	<title>Code Homework 3&mdash;Armando Garcia</title>
+	<title>Week 10 Exercises&mdash;Armando Garcia</title>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
 </head>
