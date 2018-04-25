51cb35d72d91eb145c451f6eb6b8d51d40e4f66f
Armando G <armgar@gmail.com>
2018-04-25 00:05:44 -0400

    Added queries to index.php

--- agarci27/html_head.php
+++ agarci27/html_head.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="Database Final Project for LIS-638 Web Development">
 		<title>Database&mdash;Armando Garcia</title>
-		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
 		<link href="css/style.css" rel="stylesheet">
 	</head>
 <body>
--- agarci27/index.php
+++ agarci27/index.php
@@ -12,5 +12,59 @@ Password: <input type="password" name="upass">
 <input type="submit" name="submit" value="Login!">
 
 <?php
+$hn = 'localhost';
+$db = 'agarci27';
+$un = 'armgar';
+$pw = 'armgar';
+
+# Make the connection to mysql using the credentials above
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die($conn->connect_error);
+
+# Construct the query for the results we'd like
+$query = "SELECT * FROM to_do";
+
+# Run our query, making sure we received results back
+$result = $conn->query($query);
+if (!$result) die($conn->error);
+
+# Determine the number of rows returned so we can loop through them
+$rows = $result->num_rows;
+echo "<p>Here are your to-dos for next week:</p>";
+# Get and print out each row returned from the database
+while ($row = $result->fetch_assoc()) {
+	echo "<table><tr>";
+		echo "<td>".$row["to_do_id"]."</td><td>".$row["date_due"]."</td><td>".
+			$row["time_due"]."</td><td>".$row["contact"]."</td><td>".$row["description"].
+			"</td><td>".$row["priority"]."</td>";
+	echo "</tr></table>";
+}
+
+# Construct the query for the results we'd like
+$query = "SELECT * FROM to_do WHERE priority = 2";
+
+# Run our query, making sure we received results back
+$result = $conn->query($query);
+if (!$result) die($conn->error);
+
+# Determine the number of rows returned so we can loop through them
+$rows = $result->num_rows;
+
+# Get and print out each row returned from the database
+while ($row = $result->fetch_assoc()) {
+	echo "<table class='listall'>";
+		echo "<td>".$row["to_do_id"]." ".$row["priority"]." ".$row["date_due"]." ";
+		echo $row["time_due"]." ".$row["contact"]." ".$row["description"]."<br>";
+	echo "</td></table>";
+}
+
+# close the database connection
+$result->close();
+$conn->close();
+?>
+
+
+<?php
 	include "footer.php";
 ?>
+
